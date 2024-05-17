33fde54910105bef39bf60047b509b8c37ba25e9
reZerOR <gkraihan14k@gmail.com>
2024-05-17 20:03:59 +0600

    not found router function

--- express-js/dist/app.js
+++ express-js/dist/app.js
@@ -6,8 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const useRouter = express_1.default.Router();
+app.use("/api/user", useRouter);
+useRouter.get("/create", (req, res) => {
+    res.json({ success: true,
+        message: "User is created successfully",
+        data: "ai ne data"
+    });
+});
 app.get("/", (req, res) => {
     res.send("Hello developer!");
-    res;
+});
+app.all("*", (req, res) => {
+    res.status(404).json({
+        message: "you failde you parents"
+    });
 });
 exports.default = app;
--- express-js/src/app.ts
+++ express-js/src/app.ts
@@ -2,9 +2,25 @@ import express, { Request, Response } from "express";
 const app = express();
 app.use(express.json());
 
+const useRouter = express.Router()
+app.use("/api/user", useRouter)
+
+useRouter.get("/create", (req: Request, res: Response)=>{
+  res.json({success: true,
+    message: "User is created successfully",
+    data: "ai ne data"
+  })
+})
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello developer!");
-  res;
 });
+
+
+app.all("*", (req:Request, res: Response)=>{
+  res.status(404).json({
+    message: "you failde you parents"
+  })
+})
 
 export default app;
