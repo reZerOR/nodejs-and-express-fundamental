88e6341dbf5383a9ba2386816269e713fbd28f72
reZerOR <gkraihan14k@gmail.com>
2024-05-27 20:16:13 +0600

    added router folder

--- mongoose/src/app.ts
+++ mongoose/src/app.ts
@@ -1,9 +1,8 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import { globalErrorHandler } from './app/middlwares/globalErrorHandler';
 import { notFound } from './app/middlwares/notFound';
+import router from './app/routes';
 const app: Application = express();
 const port = 3000;
 
@@ -11,8 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/v1/student', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
