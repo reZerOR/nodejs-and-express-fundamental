b97ae47ad8d1eef53d3a187dbc144adb72abc094
reZerOR <gkraihan14k@gmail.com>
2024-06-11 17:46:46 +0600

    zod error and mongoose error

--- mongoose/src/app/middlwares/globalErrorHandler.ts
+++ mongoose/src/app/middlwares/globalErrorHandler.ts
@@ -1,4 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
+import { handleZodError } from '../errors/handleZodError';
+import { MongooseError } from 'mongoose';
+import { handleValidationError } from './handleValidationError';
 
 export const globalErrorHandler = (
   err: any,
@@ -6,12 +12,34 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something went wrong';
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'something went wrong';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }else if (err?.name === 'ValidationError'){
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    // err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
--- mongoose/src/app/modules/academicDepartment/academicDepartment.route.ts
+++ mongoose/src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -7,9 +7,9 @@ const router = Router();
 
 router.post(
   '/create-academic-department',
-  validateRequest(
-    AcademicDepartmentValidation.academicDepartementValidationSchema
-  ),
+  // validateRequest(
+  //   AcademicDepartmentValidation.academicDepartementValidationSchema
+  // ),
   academicDepartmentController.createAcademicDepartment
 );
 
